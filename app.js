990986e624e9f7e80acfa0fc6ec74026da57f0e3
Lee <95179300+Lee-42@users.noreply.github.com>
2024-11-28 14:57:00 +0800

    fix

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ App({
   onLaunch: function(){
     //小程序初始化执行一次onLaunch,
     console.log('app------onLaunch');
+    console.log('app------onLaunch');
+    console.log('app------onLaunch');
+    console.log('app------onLaunch');
+    console.log('app------onLaunch');
+    console.log('app------onLaunch');
+    console.log('app------onLaunch');
   },
   globalData:{ //全局数据
     userInfo: null
